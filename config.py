3fa81dd132aae288c458c2b4038d5431df868e86
sadisaharshana2038 <sadisaharshana2038@gmail.com>
2026-02-23 21:26:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,14 @@ import os
 import os
 
 # Telegram Bot Token (Get it from @BotFather)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8086741734:AAH6RCSciYO9ymqqJW6gATrvK4IMcmYMfgQ")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8689399705:AAFx-zd_e4jP1VH-D_DKHteUQsF8NxQ5Z80")
 
 # Google Drive Folder ID (The folder where files will be uploaded)
-DRIVE_FOLDER_ID = os.environ.get("DRIVE_FOLDER_ID", "1ZNwWkXJWmCi3iVVH02Y_y78yJl9qtTp5")
+DRIVE_FOLDER_ID = os.environ.get("DRIVE_FOLDER_ID", "1EdiDt5hNL0zKC86pWlidi8B-r7M5dMW3")
 
 # Path to the credentials file (client_secret.json)
 CREDENTIALS_FILE = "credentials.json"
 
 # Path to the token file (token.json)
 TOKEN_FILE = "token.json"
+
